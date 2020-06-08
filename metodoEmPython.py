85b04a31fea0e40e74786a3b30eabf17c442663e
Mario Andre <mario_andre.l.a@hotmail.com>
2020-06-08 14:58:15 -0300

    first commit

---
+++ metodoEmPython.py
@@ -0,0 +1,15 @@
+
+def metodo():
+	print("")
+	print("Método executado!")
+
+
+
+def main():
+	metodo()
+              
+
+# Executa o programa (O programa começa aqui)
+if __name__ == "__main__":
+    main()
+
